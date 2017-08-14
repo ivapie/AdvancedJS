bcb9491fd5e59c511874f309def60a2c82a56b7b
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2017-08-14 17:49:58 -0600

    let example

--- ES6/3.Lets/example.js
+++ ES6/3.Lets/example.js
@@ -1,14 +1,46 @@
 // Constants
 // Before
 
-Object.defineProperty(typeof global === 'object' ? global : window, PI, {
-  value: 3.14,
-  enumerable: true,
-  writable: false,
-  configurable: false
-});
+var i, x, y;
 
+for (i = 0; i < a.length; i++) {
+  x = a[i];
+}
+
+for (i = 0; i < b.length; i++) {
+  y = b[i];
+}
+
+var callbacks = [];
+for (var i = 0; i <= 2; i++) {
+  (function(i){
+    callbacks[i] = function(){
+      return i * 2;
+    };
+  })(i);
+}
+
+callbacks[0](); // 0
+callbacks[1](); // 2
+callbacks[2](); // 4
 
 // After
 
-const PI = 3.14;
+for (let i = 0; i < a.length; i++) {
+  let x = a[i];
+}
+
+for (let i = 0; i < b.length; i++) {
+  let y = b[i];
+}
+
+let callbacks = [];
+for (let i = 0; i <= 2; i++) {
+  callbacks[i] = function(){
+    return i * 2;
+  };
+}
+
+callbacks[0](); // 0
+callbacks[1](); // 2
+callbacks[2](); // 4
