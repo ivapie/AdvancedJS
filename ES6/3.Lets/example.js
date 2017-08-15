ebc3d161274c66662668cc62d3b0e7704bf1a713
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2017-08-14 18:39:11 -0600

    arrow functions

--- ES6/3.Lets/example.js
+++ ES6/3.Lets/example.js
@@ -1,4 +1,4 @@
-// Constants
+// Let keywordds
 // Before
 
 var i, x, y;
--- ES6/3.Lets/index.js
+++ ES6/3.Lets/index.js
@@ -1,34 +1,20 @@
-// constant definition √
-const MY_FAV = 7;
-
-MY_FAV = 9; // throw error :(
-
-console.log(`My favorite number is ${MY_FAV}`); // 7
-
-const MY_FAV = 20; // throw error :(
-
-var MY_FAV = 20; // throw error :(
-
-let MY_FAV = 20; // throws error :(
-
-if (MY_FAV === 7) {
-  let MY_FAV = 20; // loose scope √
-  console.log(`My favorite number is ${MY_FAV}`); // 20
-  var MY_FAV = 20; // error :(
+function varTest(){
+  var x =1;
+  if (true) {
+    var x = 2;
+    console.log(x); // 2
+  }
+  console.log(x); // 2
 }
 
-console.log(`My favorite number is ${MY_FAV}`); // 7
+function letTest(){
+  let x =1;
+  if (true) {
+    let x = 2;
+    console.log(x) // 2
+  }
+  console.log(x); // 1 <--- WOOOW
+}
 
-const FOO; // missing initializer :(
-
-const MY_OBJECT = {'KEY': 'value'}; // √
-
-MY_OBJECT = {'OTHER_KEY': 'other_value'} // Can't override :(
-
-MY_OBJECT.new_key = 'new_value'; // This works √
-
-const MY_ARRAY = ['A', 'B']; // √
-
-MY_ARRAY.push('X'); // √ ['X']
-
-MY_ARRAY = ['C']; // Error :(
+varTest();
+letTest();
